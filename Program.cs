c707c647b7f197c7521000073c1ea18ef5ea691c
Wellington Junior <wellingtonjsantosjunior@gmail.com>
2026-07-28 08:39:09 -0300

    Desabilita reload appsettings Render

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ using Google.Apis.Auth.OAuth2;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.Sources
+    .OfType<Microsoft.Extensions.Configuration.Json.JsonConfigurationSource>()
+    .ToList()
+    .ForEach(x => x.ReloadOnChange = false);
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 var jwtKey = builder.Configuration["Jwt:Key"]!;
 var jwtIssuer = builder.Configuration["Jwt:Issuer"]!;
